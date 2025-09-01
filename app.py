5bb8c09f39509e125686c517f6a57dca313e18ab
Shubham Nayan <shubh.nyn4u@gmail.com>
2025-09-01 12:33:51 +0530

    Fix BadRequestError and improve app stability

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader, PyPDFLoad
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_groq import ChatGroq, Groq
+from langchain_groq import ChatGroq
 from groq import BadRequestError
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
